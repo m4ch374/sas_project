c5a195f84e468c7a4e20686abf234aa6792205a1
m4ch374 <spiner47@gmail.com>
2023-07-14 22:47:33 +1000

    example(frontend): added fetch requests to test backend examples

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const route = "http://127.0.0.1:8000/models"
+  useEffect(() => {
+    fetch(route, {
+      method: "GET",
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+
+  // lazy lol
+  const route1 = "http://127.0.0.1:8000/models/accurate"
+  useEffect(() => {
+    fetch(route1, {
+      method: "POST",
+      body: JSON.stringify({
+        "input": 1
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
 
   return (
     <>
